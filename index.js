631560940df68ab57306a1838e16a7e2d3f3be2d
ismail debbarh <usamdh07@gmail.com>
2023-10-17 14:00:48 +0100

    perf: update just the changed cells not the entire grid

--- index.js
+++ index.js
@@ -111,6 +111,71 @@ function isSnakeCell(pos) {
   return [false, isSnakeHead];
 }
 
+function resetSnakeCell(cell) {
+  const cellDot = document.createElement("span");
+  cellDot.className = "cell__dot";
+  cell.appendChild(cellDot);
+  cell.classList.remove("row__cell--snake-body-cell");
+  cell.classList.remove("row__cell--snake-head-cell");
+}
+
+function changeCellToSnakeCell(cur, isHead = false) {
+  const [bodyCol, bodyRow] = cur.value;
+  const newBodyCell = document.querySelector(
+    `[cell-pos="${bodyCol},${bodyRow}"]`,
+  );
+  const newBodyCellDot = newBodyCell.querySelector(".cell__dot");
+
+  if (newBodyCellDot) {
+    newBodyCellDot.remove();
+  }
+
+  if (isHead) {
+    newBodyCell.classList.add("row__cell--snake-head-cell");
+  } else {
+    newBodyCell.classList.add("row__cell--snake-body-cell");
+  }
+}
+
+function updateGridSnakeCells() {
+  let cur = snakeBody.head;
+
+  const curSnakeBodyCells = document.getElementsByClassName(
+    "row__cell--snake-body-cell",
+  );
+
+  const curSnakeHeadCell = document.querySelector(
+    ".row__cell--snake-head-cell",
+  );
+
+  Array.from(curSnakeBodyCells).forEach((cell) => {
+    resetSnakeCell(cell);
+  });
+
+  resetSnakeCell(curSnakeHeadCell);
+
+  while (cur.next) {
+    changeCellToSnakeCell(cur);
+    cur = cur.next;
+  }
+
+  changeCellToSnakeCell(cur, true);
+}
+function updateGridBeatCell() {
+  const [beatCol, beatRow] = beatPos;
+  const curBeatCell = document.querySelector(".row__cell--beat-cell");
+  const newBeatCell = document.querySelector(
+    `[cell-pos="${beatCol},${beatRow}"]`,
+  );
+  curBeatCell.classList.remove("row__cell--beat-cell");
+  newBeatCell.classList.add("row__cell--beat-cell");
+}
+
+function updateGrid() {
+  updateGridBeatCell();
+  updateGridSnakeCells();
+}
+
 function keyPressHandler(e) {
   const dir = e.key;
   if (!DIRS.includes(dir)) {
@@ -118,7 +183,7 @@ function keyPressHandler(e) {
   }
   moveSnake(dir);
   checkIfBeatEaten();
-  renderGrid();
+  updateGrid();
 }
 
 function renderGrid() {
@@ -130,30 +195,30 @@ function renderGrid() {
   for (let i = 0; i < ROWS; ++i) {
     const rowContainer = document.createElement("div");
     rowContainer.className = "grid__row";
+
     for (let j = 0; j < COLS; ++j) {
-      const [_isSnakeCell, isSnakeHead] = isSnakeCell([j, i]);
+      const [, isSnakeHead] = isSnakeCell([j, i]);
       const cell = document.createElement("div");
+
       cell.className = "row__cell";
       cell.setAttribute("cell-pos", `${j},${i}`);
+
       if (i === beatRow && j == beatCol) {
         cell.classList.add("row__cell--beat-cell");
-      } else if (_isSnakeCell) {
-        if (isSnakeHead) {
-          cell.classList.add("row__cell--snake-head-cell");
-        } else {
-          cell.classList.add("row__cell--snake-body-cell");
-        }
+      } else if (isSnakeHead) {
+        cell.classList.add("row__cell--snake-head-cell");
       }
-      if (!_isSnakeCell) {
+
+      if (!isSnakeHead) {
         const cellDot = document.createElement("span");
         cellDot.className = "cell__dot";
         cell.appendChild(cellDot);
       }
+
       rowContainer.appendChild(cell);
     }
     gridContainer.appendChild(rowContainer);
   }
-  document.body.innerHTML = "";
   document.body.appendChild(gridContainer);
 }
 
